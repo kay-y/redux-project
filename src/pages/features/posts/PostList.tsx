f1fda4395f8b6ac46e9e568b1eec08868608e2e1
kay-y <kayodeolanrewaju53@gmail.com>
2023-07-28 16:23:09 +0100

    new commit

---
+++ src/pages/features/posts/PostList.tsx
@@ -0,0 +1,24 @@
+import {  useSelector } from "react-redux";
+import { selectAllPosts } from "./postSlice";
+
+
+const PostList = () => {
+    const posts = useSelector(selectAllPosts)
+
+    const renderedPosts = posts.map((post: any) => (
+        <article key={post.id} className="border w-[300px] h-full mx-auto my-3 text-white  transition ease-in-out delay-150 bg-black hover:-translate-y-1 hover:scale-110 hover:bg-gradient-to-br from-black to-[#111111] duration-300 cursor-pointer hover:shadow-md hover:shadow-black  ">
+            <h2 className="text text-lg font-bold">{post.title}</h2>
+            <p>{post.content.substring(0, 100)}</p>
+        </article>
+    ))
+  return (
+    <section  className=" text-center  ">
+        <h2 className="text-2xl font-sans font-extrabold">Posts</h2>
+        <div className="flex justify-center flex-col">
+        {renderedPosts}
+      </div>
+    </section>
+  )
+}
+
+export default PostList
---
+++ src/pages/features/posts/postSlice.tsx
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [
+    {id: '1', title: 'Learning Redux Toolkit', content: 'Lorem ipsum dolor sit amet consectetur adipiscing elit viverra'},
+    {id: '2', title: 'Slice...', content: 'Lorem ipsum dolor sit amet consectetur adipiscing elit viverra'}
+
+]
+
+const postSlice = createSlice({
+    name: 'posts',
+    initialState,
+    reducers: {
+        addPost: (state, action) => {
+            state.push(action.payload)
+        }
+    }
+})
+
+export const selectAllPosts = (state: any) => state.posts;
+
+export default postSlice.reducer
